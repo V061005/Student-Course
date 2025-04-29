899075fb22a1747d0c5f83e3033ec15da04085f7
ruksanabetgeri <ruksanabetageri300@gmail.com>
2025-04-29 15:28:13 +0530

    Save

---
+++ Student.java
@@ -0,0 +1,13 @@
+
+public class Student {
+    public static void main(String[] args) {
+        String name="Vaishnavi";
+        int birthyear=2005;
+        int currentyear=2025;
+
+        int age= currentyear-birthyear;
+
+        System.err.println("Name:"+name);
+        System.out.println("Age:"+age);
+    }
+}
